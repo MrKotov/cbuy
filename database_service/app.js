7429b52b594d360017e3b10172bbea5be7006334
Georgi Kotov <gkotov94@gmail.com>
2018-01-14 19:29:17 +0200

    correct ports

--- database_service/app.js
+++ database_service/app.js
@@ -1,4 +1,4 @@
 const http = require('http')
 const server = http.createServer(require('./handler'))
 const port = parseInt(process.argv.slice(2))
-server.listen(8082)
+server.listen(port)
--- file_service/app.js
+++ file_service/app.js
@@ -1,4 +1,4 @@
 const http = require('http')
 const server = http.createServer(require('./handler'))
 const port = parseInt(process.argv.slice(2))
-server.listen(8083)
+server.listen(port)
